0973aad8821bc5a85db5abacdae8597d18b29344
hemprakashpatidar <57744657+hemprakashpatidar@users.noreply.github.com>
2025-07-26 01:57:06 +0530

    Update notion.js

--- api/notion.js
+++ api/notion.js
@@ -1,6 +1,10 @@
 export default async function handler(req, res) {
   const { NOTION_SECRET, DATABASE_ID } = process.env;
 
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   if (req.method !== 'GET') {
     return res.status(405).json({ message: 'Only GET allowed' });
   }
